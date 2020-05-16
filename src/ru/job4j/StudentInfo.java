c06deaaea8b6c28cc54a7caf99a15b1b6a2356e5
ssstre <65305795+ssstre@users.noreply.github.com>
2020-05-16 22:46:51 +0500

    3.3. Разбор первой программы.[#291149]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main (String[] args) {
+        System.out.println("Lezhnev Andrey");
+        System.out.println("26.02.1988");
+    }
+}
