d0dd3dfdeda4efdcc6aca00329acd5f97656b048
ssstre <65305795+ssstre@users.noreply.github.com>
2020-05-18 21:06:55 +0500

    9.1. String[#291140]

--- src/ru/job4j/StudentInfo.java
+++ src/ru/job4j/StudentInfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         System.out.println("Lezhnev Andrey");
         System.out.println("26.02.1988");
     }
--- src/ru/job4j/calculator/Fit.java
+++ src/ru/job4j/calculator/Fit.java
@@ -3,7 +3,7 @@ package ru.job4j.calculator;
 public class Fit {
 
         public static double manWeight(double height) {
-            double rsl = (height - 100) * 1.15 ;
+            double rsl = (height - 100) * 1.15;
             return rsl;
         }
 
