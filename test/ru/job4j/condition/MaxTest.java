64c9752a9d2af92893c68940147fca94076cbe1c
ssstre <65305795+ssstre@users.noreply.github.com>
2020-05-28 01:45:46 +0500

    4.2. Тернарное сравнение[189#291098]test

--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -9,7 +9,26 @@ public class MaxTest {
 
     @Test
     public void whenMax1To2Then2() {
-        int result = Max.max(1, 2);
-        assertThat(result, is(2));
+        int first = 1;
+        int second = 2;
+        int expected = 2;
+        int out = Max.max(first, second);
+        Assert.assertEquals(expected, out);
+    }
+    @Test
+    public void whenMax2ToThen1() {
+        int first = 2;
+        int second = 1;
+        int expected = 2;
+        int out = Max.max(first, second);
+        Assert.assertEquals(expected, out);
+    }
+    @Test
+    public void whenEquals1To2Then2() {
+        int first = 2;
+        int second = 2;
+        int expected = 2;
+        int out = Max.max(first, second);
+        Assert.assertEquals(expected, out);
     }
 }
