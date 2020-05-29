03b8032bff3e60e422cd5e4e5579c67a596aeb22
ssstre <65305795+ssstre@users.noreply.github.com>
2020-05-29 13:11:40 +0500

    4.4. Максимум из трех чисел[190#291099]

--- src/ru/job4j/condition/MultiMax.java
+++ src/ru/job4j/condition/MultiMax.java
@@ -4,10 +4,7 @@ public class MultiMax {
     public static  int max(int first, int second, int third) {
         int rsl1;
         rsl1 = first >= second ? first : second;
-        int rsl2;
-        rsl2 = first >= third ? first : third;
-        int rsl;
-        rsl = rsl1 >= rsl2 ? rsl1 : rsl2;
-        return rsl;
+        return rsl1 >= third ? rsl1 : third;
+
     }
 }
