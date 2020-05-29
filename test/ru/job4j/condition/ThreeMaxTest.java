22c68aefb9bf4466f6c399dafea170ed4fe64153
ssstre <65305795+ssstre@users.noreply.github.com>
2020-05-29 14:02:40 +0500

    4.5. Отладка программы в IDEA[159464#291093]

--- test/ru/job4j/condition/ThreeMaxTest.java
+++ test/ru/job4j/condition/ThreeMaxTest.java
@@ -16,6 +16,70 @@ public class ThreeMaxTest {
         int out = ThreeMax.max(first, second, third);
         Assert.assertEquals(expected, out);
     }
+    @Test
+    public void secondMax() {
+        int first = 10;
+        int second = 50;
+        int third = 1;
+        int expected = 50;
+        int out = ThreeMax.max(first, second, third);
+        Assert.assertEquals(expected, out);
+
+    }
+
+    @Test
+    public void thirdMax() {
+        int first = 1;
+        int second = 5;
+        int third = 100;
+        int expected = 100;
+        int out = ThreeMax.max(first, second, third);
+        Assert.assertEquals(expected, out);
+
+    }
+
+    @Test
+    public void allEq() {
+        int first = 1;
+        int second = 1;
+        int third = 1;
+        int expected = 1;
+        int out = ThreeMax.max(first, second, third);
+        Assert.assertEquals(expected, out);
+
+    }
+
+    @Test
+    public void firstEqSecond() {
+        int first = 10;
+        int second = 10;
+        int third = 1;
+        int expected = 10;
+        int out = ThreeMax.max(first, second, third);
+        Assert.assertEquals(expected, out);
+
+    }
+
+    @Test
+    public void firstEqThird() {
+        int first = 100;
+        int second = 1;
+        int third = 100;
+        int expected = 100;
+        int out = ThreeMax.max(first, second, third);
+        Assert.assertEquals(expected, out);
+
+    }
+
+    @Test
+    public void secondEqThird() {
+        int first = 11;
+        int second = 100;
+        int third = 100;
+        int expected = 100;
+        int out = ThreeMax.max(first, second, third);
+        Assert.assertEquals(expected, out);
+    }
 
 
 
