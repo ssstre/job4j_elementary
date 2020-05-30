36d285830bab9188c4ebd2b0e2d2f156892c1dcf
ssstre <65305795+ssstre@users.noreply.github.com>
2020-05-30 15:26:00 +0500

    5.4. Протеиновая диета.[156313#291179]

---
+++ test/ru/job4j/loop/FitnessTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.loop;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FitnessTest {
+
+    @Test
+        public void whenIvanGreatNik() {
+        int expected = 0;
+        int month = Fitness.calc(95, 90);
+            Assert.assertEquals(month, expected);
+    }
+    @Test
+    public void whenIvanLessByOneNik() {
+        int expected = 1;
+        int month = Fitness.calc(90, 95);
+        Assert.assertEquals(month, expected);
+    }
+
+    @Test
+    public void whenIvanLessByFewNik() {
+        int expected = 2;
+        int month = Fitness.calc(50, 90);
+        Assert.assertEquals(month, expected);
+    }
+}
