7d04c28e2fba9bef544932cee3d238bfdc090b09
ssstre <65305795+ssstre@users.noreply.github.com>
2020-06-01 22:35:39 +0500

    5.8. JavaDoc. Code convention[4087#291190]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -1,15 +1,38 @@
+/**
+ * Converter
+ */
 package ru.job4j.converter;
 
+/**
+ * Class Converter решение задачи части 1, урок 5.
+ * @author lezhnev
+ * @since 20.05.2020
+ */
 public class Converter {
+    /**
+     * Метод rubleToEuro для конвектирования рублей в евро.
+     * @param value переменная для ввода количества рублей.
+     * @return rsl результат конвектирования в евро.
+     */
     public static int rubleToEuro(int value) {
         int rsl = value / 70;
         return rsl;
     }
+    /**
+     * Метод rubleToDollar для конвектирования рублей в доллары
+     * @param value переменная для ввода количества рублей.
+     * @return rsl1 результат конвектирования в доллары.
+     */
     public static int rubleToDollar(int value) {
         int rsl1 = value / 60;
         return rsl1;
     }
 
+    /**
+     * Метод для тестирования методов rubleToEuro и rubleToDollar
+     * Main
+     * @param args - args
+     */
     public static void main(String[] args) {
         int expected = 2;
         int euro = Converter.rubleToEuro(140);
