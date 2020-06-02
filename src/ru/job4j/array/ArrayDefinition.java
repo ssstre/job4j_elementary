8671bea20521a227046efe49c5b54bcb516d9951
ssstre <65305795+ssstre@users.noreply.github.com>
2020-06-02 20:52:00 +0500

    6.0.2. Заполнение массива.[226300#291118]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,17 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+
+        String[] names = new String[4];
+        names[0] = "Iosif Stalin";
+        names[1] = "Il'ich Lenin";
+        names[2] = "Georgii Malenkov";
+        names[3] = "Nikita Hryshev";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+
+
     }
 }
--- src/ru/job4j/loop/Slash.java
+++ src/ru/job4j/loop/Slash.java
@@ -1,30 +1,30 @@
 package ru.job4j.loop;
 
 public class Slash {
-    public static void draw(int size) {
-        for (int row = 0; row < size; row++) {
-            for (int cell = 0; cell < size; cell++) {
-              if (size == 3) {
-                  boolean right = row + cell == 4;
-              } else { boolean right = row + cell == 2;
-                }
-                if (row == cell) {
-                    System.out.print("0");
-                } else if (right) {
-                    System.out.print("0");
-                } else {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
-        }
-        System.out.println();
-    }
-
-    public static void main(String[] args) {
-        System.out.println("Draw by 3");
-        draw(3);
-        System.out.println("Draw by 5");
-        draw(5);
-    }
+    //ublic static void draw(int size) {
+    //   for (int row = 0; row < size; row++) {
+    //       for (int cell = 0; cell < size; cell++) {
+    //         if (size == 3) {
+    //             boolean right = row + cell == 4;
+    //         } else { boolean right = row + cell == 2;
+    //           }
+    //           if (row == cell) {
+    //               System.out.print("0");
+    //           } else if (right) {
+    //               System.out.print("0");
+    //           } else {
+    //               System.out.print(" ");
+    //           }
+    //       }
+    //       System.out.println();
+    //   }
+    //   System.out.println();
+    //
+    //
+    //ublic static void main(String[] args) {
+    //   System.out.println("Draw by 3");
+    //   draw(3);
+    //   System.out.println("Draw by 5");
+    //   draw(5);
+    //
 }
