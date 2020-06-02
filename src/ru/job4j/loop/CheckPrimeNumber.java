d80edbff1055d629be356ff5ef9fdcfcfa48ed82
ssstre <65305795+ssstre@users.noreply.github.com>
2020-06-02 18:28:34 +0500

    5.5. Простое число[171694#291182]

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -4,15 +4,12 @@ public class CheckPrimeNumber {
     public static boolean check(int number) {
 
         boolean p = number > 1;
-        int num = 2;
-        while (num < number) {
-            if ((number % num) == 0) { // если число делется без остатка на любое число кроме 1 и number то число не простое
-
+        for (int i = 2; i < number; i++) {
+            if ((number % i) == 0) { // если число делется без остатка на любое число кроме 1 и number то число не простое
                 p = false;
-            break;
+                break;
             }
-            num++;
         }
-        return p; // выводим результат
+        return p;// выводим результат
     }
 }
