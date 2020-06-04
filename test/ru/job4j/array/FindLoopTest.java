ff8224623c575dae9814103e76874576920a1857
ssstre <65305795+ssstre@users.noreply.github.com>
2020-06-05 01:36:56 +0500

    6.1. Классический поиск перебором.[33489#291104]

---
+++ test/ru/job4j/array/FindLoopTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FindLoopTest {
+
+   @Test
+   public void indexOf() {
+       FindLoop find = new FindLoop();
+       int[] input = new int[] {5, 10, 3};
+       int value = 5;
+       int expected = 0;
+       int result = FindLoop.indexOf(input, value);
+
+       Assert.assertEquals(result, expected);
+   }
+    public void indexOf1() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {5, 10, 3};
+        int value = 6;
+        int expected = -1;
+        int result = FindLoop.indexOf(input, value);
+
+        Assert.assertEquals(result, expected);
+    }
+}
