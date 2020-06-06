a814c3943a3a78625bb3bc2e4765d7eb97c267dd
ssstre <65305795+ssstre@users.noreply.github.com>
2020-06-07 03:26:57 +0500

    6.5. Поиск индекса в диапазоне.[156318#291111]

--- src/ru/job4j/array/FindLoop.java
+++ src/ru/job4j/array/FindLoop.java
@@ -1,10 +1,10 @@
 package ru.job4j.array;
 
 public class FindLoop {
-    public static int indexOf(int[] data, int el) {
+    public static int indexOf(int[] data, int el, int start, int finish) {
         int rst = -1; // если элемента нет в массиве, то возвращаем -1.
 
-        for (int index = 0; index < data.length; index++ ) {
+        for (int index = start; index < finish; index++ ) {
             if (data[index] == el) {
                 rst = index;
                 break;
--- test/ru/job4j/array/FindLoopTest.java
+++ test/ru/job4j/array/FindLoopTest.java
@@ -7,22 +7,24 @@ import static org.junit.Assert.*;
 
 public class FindLoopTest {
 
-   @Test
-   public void indexOf() {
-       FindLoop find = new FindLoop();
-       int[] input = new int[] {5, 10, 3};
-       int value = 5;
-       int expected = 0;
-       int result = FindLoop.indexOf(input, value);
-       Assert.assertEquals(result, expected);
-   }
-    public void indexOf1() {
-        FindLoop find = new FindLoop();
-        int[] input = new int[] {5, 10, 3};
-        int value = 6;
-        int expected = -1;
-        int result = FindLoop.indexOf(input, value);
-
-        Assert.assertEquals(result, expected);
+    @Test
+    public void whenFind3() {
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 3;
+        Assert.assertEquals(result, expect);
     }
+    @Test
+    public void indexOf1() {
+      int[] input = new int[] {5, 2, 10, 2, 4};
+      int value = 1;
+      int start = 2;
+      int finish = 4;
+      int result = FindLoop.indexOf(input, value, start, finish);
+      int expect = -1;
+      Assert.assertEquals(result, expect);
+  }
 }
